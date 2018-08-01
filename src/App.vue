23d61ac2915f24dbe4bae7b14f354acebc534464
Jason Tseng <jason.f.tseng@gmail.com>
2018-08-01 09:46:59 -0400

    added fetch of data on created hook, added card component and rough template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <card v-for="(post, key) in displayPosts" :key="key" :post="post"></card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      posts: null,
+      currentPage: 0
+    }
+  },
+  created() {
+    console.log('created')
+    fetch('http://graphql.dosomething.org/graphql?query={posts(campaignId: "7978") { createdAt reactions url (w: 300, h: 300) text user { firstName}}}')
+      .then((resp) => {
+        return resp.json()
+      })
+      .then((json) => {
+        this.posts = json.data.posts
+      })
+      .catch(error => console.log(error))
+  },
+  computed: {
+    pages() {
+      let tempArray = [];
+      if (this.posts) {
+        for (let i = 0; i < this.posts.length; i += 9) {
+            console.log(i, this.posts, tempArray)
+            let page = this.posts.slice(i, i + 9);
+            tempArray.push(page);
+        }
+      }
+      return tempArray;
+    },
+    displayPosts() {
+      return this.pages[this.currentPage]
+    }
   }
 }
 </script>
@@ -21,7 +53,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="card">
+        <div class="image" :style="imageStyle" ></div>
+        <div class="content" >
+            <div class="text" >{{post.text}}</div>
+            <div class="info" >by {{post.user.firstName}} at {{post.createdAt}} </div>
+            <div class="likes" >{{post.reactions}} Likes </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    props: ["post"],
+    data() {
+        return {
+            imageStyle: {
+                backgroundImage: `url(${this.post.url})`
+            }
+        }
+    }    
+}
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        width: 300px;
+        height: 400px;
+        .image {
+            width: 300px;
+            height: 300px;
+            background-position: center;
+            background-size: contain;
+        }
+        .content {
+            box-sizing: border-box;
+            padding: 16px 0;
+        }
+    }
+
+</style>
