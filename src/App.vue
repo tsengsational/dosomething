33bf69f1ede81dd73e984aa1c7784bb904b35ad1
Jason Tseng <jason.f.tseng@gmail.com>
2018-08-01 12:59:14 -0400

    added mobile-first styling on App and Card components and navigation buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <card v-for="(post, key) in displayPosts" :key="key" :post="post"></card>
+    <div class="title">
+      <div class="title-content">
+        {{campaignTitle}}
+      </div>
+    </div>
+    <div class="cards-container" >
+      <card v-for="(post, key) in displayPosts" :key="key" :post="post"></card>
+    </div>
+    <div class="controls-container">
+      <button class="previous btn" @click.prevent="handleControlClick" >Previous</button>
+      <button class="next btn" @click.prevent="handleControlClick" >Next</button>
+    </div>
   </div>
 </template>
 
@@ -15,12 +26,13 @@ export default {
   data() {
     return {
       posts: null,
-      currentPage: 0
+      currentPage: 0,
+      campaignTitle: "#MyBigRegret"
     }
   },
   created() {
     console.log('created')
-    fetch('http://graphql.dosomething.org/graphql?query={posts(campaignId: "7978") { createdAt reactions url (w: 300, h: 300) text user { firstName}}}')
+    fetch('http://graphql.dosomething.org/graphql?query={posts(campaignId: "7978") {status createdAt reactions url (w: 300, h: 300) text user { firstName}}}')
       .then((resp) => {
         return resp.json()
       })
@@ -33,9 +45,8 @@ export default {
     pages() {
       let tempArray = [];
       if (this.posts) {
-        for (let i = 0; i < this.posts.length; i += 9) {
-            console.log(i, this.posts, tempArray)
-            let page = this.posts.slice(i, i + 9);
+        for (let i = 0; i < this.acceptedPosts.length; i += 9) {
+            let page = this.acceptedPosts.slice(i, i + 9);
             tempArray.push(page);
         }
       }
@@ -43,17 +54,92 @@ export default {
     },
     displayPosts() {
       return this.pages[this.currentPage]
+    },
+    acceptedPosts() {
+      let tempArray = [];
+      if (this.posts) {
+        tempArray = this.posts.filter(post => post.status === "ACCEPTED")
+      }
+      return tempArray
+    }
+  },
+  methods: {
+    handleControlClick(event) {
+      const type = event.target.classList[0]
+      switch (type) {
+        case "previous":
+          if (this.currentPage > 0) {
+            this.currentPage--
+          }
+          break;
+        case "next":
+          if (this.currentPage < this.pages.length -1) {
+            this.currentPage++
+          }
+        default:
+          break;
+      }
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/settings.scss';
+
+body {
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: ProximaNovaReg, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: $black;
+  margin-bottom: 60px;
+}
+
+.title {
+  width: 100vw;
+  height: 32px;
+  background-color: $purple;
+  color: $white;
+  @include title(24px);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+
+.cards-container {
+  display: flex;
+  flex-wrap:wrap;
+  width: 100vw;
+  justify-content: space-between;
+  position: relative;
   margin-top: 60px;
+}
+
+.controls-container {
+  position: absolute;
+  right: calc(5vw);
+  .btn {
+    @include button(70px, 40px, 16px);
+  }
+  .previous {
+    margin-right: 16px;
+  }
+}
+
+@media (min-width: 500px) {
+  .cards-container {
+    width: 1000px;
+    left: calc(50vw - 500px)
+  }
+  .title {
+    height: 40px;
+  }
+  .controls-container {
+    right: calc(50vw - 500px);
+  }
 }
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,7 +3,7 @@
         <div class="image" :style="imageStyle" ></div>
         <div class="content" >
             <div class="text" >{{post.text}}</div>
-            <div class="info" >by {{post.user.firstName}} at {{post.createdAt}} </div>
+            <div class="info" >by <span>{{post.user.firstName}}</span> at <span>{{createdAt}}</span> </div>
             <div class="likes" >{{post.reactions}} Likes </div>
         </div>
     </div>    
@@ -18,24 +18,43 @@ export default {
                 backgroundImage: `url(${this.post.url})`
             }
         }
+    },
+    computed: {
+        createdAt() {
+            const date = new Date(this.post.createdAt)
+            return date.toLocaleDateString()
+        }
     }    
 }
 </script>
 
 <style lang="scss" scoped>
-    .card {
+@import '../assets/settings.scss';
+
+.card {
+    width: 300px;
+    height: 400px;
+    margin-bottom: 16px;
+    border: 1px $gray solid;
+    .image {
         width: 300px;
-        height: 400px;
-        .image {
-            width: 300px;
-            height: 300px;
-            background-position: center;
-            background-size: contain;
+        height: 300px;
+        background-position: center;
+        background-size: contain;
+    }
+    .content {
+        box-sizing: border-box;
+        padding: 16px;
+        background-color: $dark-gray;
+        color: $white;
+        height: 100px;
+        .text {
+            @include title(16px);
         }
-        .content {
-            box-sizing: border-box;
-            padding: 16px 0;
+        .info span {
+            @include title(16px);
         }
     }
+}
 
 </style>
